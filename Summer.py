5a8f908c834f6a1dcd9d8187054c61e179d8640d
Darknight47 <farhadgunners@gmail.com>
2026-03-09 10:10:01 +0100

    implementation

---
+++ Summer.py
@@ -0,0 +1,32 @@
+"""
+
+---------------------------------- Link for the challenge: https://codeforces.com/problemset/problem/44/A ---------------------------------
+
+Indian summer is such a beautiful time of the year! A girl named Alyona is walking in the forest and picking a bouquet from fallen leaves. 
+Alyona is very choosy — she doesn't take a leaf if it matches the color and the species of the tree of one of the leaves she already has. Find out how many leaves Alyona has picked.
+
+Input
+The first line contains an integer n (1 ≤ n ≤ 100) — the number of leaves Alyona has found. The next n lines contain the leaves' descriptions. 
+Each leaf is characterized by the species of the tree it has fallen from and by the color. 
+The species of the trees and colors are given in names, consisting of no more than 10 lowercase Latin letters. A name can not be an empty string. 
+The species of a tree and the color are given in each line separated by a space.
+
+Output
+Output the single number — the number of Alyona's leaves.
+
+Input:
+5
+birch yellow
+maple red
+birch yellow
+maple yellow
+maple green
+
+Output:
+4
+"""
+n = int(input())
+temp_set = set()
+for _ in range(n):
+    temp_set.add(input())
+print(len(temp_set))
